707198b144d6669d88f251bae1f794aea53ace4e
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2022-06-11 12:36:13 -0300

    Atualização01

---
+++ Script/script.js
@@ -0,0 +1,32 @@
+/*
+   
+    function alerta(){
+        alert("Olá");
+    } */
+
+    /*
+
+    function alerta(){
+        console.log("Olá");
+    } */
+
+    /*
+
+    function alerta(){
+        document.write("Olá");
+    } */
+
+    /*
+
+    function alerta(){
+        document.getElmenById("saida").innerHTML = "Olá";
+    }
+        */
+
+    /* getElmenById = pegar o elemento pelo id */
+
+    function alerta(){
+        console.log("Olá");
+        console.log("Olá");
+        console.log("Olá");
+    }
